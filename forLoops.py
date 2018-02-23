811d0063969429ddbb3fad6638e686d95dd43be2
Salvador Muñoz Manzaneda <salva17y2@gmail.com>
2018-02-23 18:43:01 +0100

    terminado los bloques loops y listas

--- forLoops.py
+++ forLoops.py
@@ -33,3 +33,44 @@ def tag_count(palabras):
 list1 = ['<greeting>', 'Hello World!', '</greeting>']
 count = tag_count(list1)
 print("Expected result: 2, Actual result: {}".format(count))
+
+
+#TODO: Implement the nearest_square function
+def nearest_square(limite):
+    """
+    The function takes an integer argument limit, and returns the largest
+    square number that is less than limit. A square number is the product
+    of an integer multiplied by itself, for example 36 is a square number
+    because it equals 6*6.
+    """
+    num = 1
+    while (num * num) <= limite:
+        ultimo_valido = num * num
+        num += 1
+
+    return ultimo_valido
+
+test1 = nearest_square(40)
+print("expected result: 36, actual result: {}".format(test1))
+
+
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
+
+news_ticker = ""
+# TODO: set news_ticker to a string that contains no more than 140 characters long.
+# HINT: modify the headlines list to verify your loop works with different inputs
+
+
+for phrase in headlines:
+    if len(news_ticker) + len(phrase) <= 140:
+        news_ticker = news_ticker + " " + phrase
+    else:
+        break
+
+
+print(len(news_ticker))
--- listas.py
+++ listas.py
@@ -21,3 +21,28 @@ recents = eclipse_dates[-3:]  #los tres ultimos
 # TODO: Modify this line so it prints the last three elements of the list
 #print(eclipse_dates)
 print(recents)
+
+
+#define the  html_list function
+def html_list(argument):
+    #CODE
+    print("<ul>");
+    for palabra in argument:
+        print("<li>" + palabra + "</li>");
+    print("</ul>");
+
+html_list(['first string', 'second string']);
+
+def starbox(width, height):
+    """print a box made up of asterisks.
+    
+    width: width of box in characters, must be at least 2
+    height: height of box in lines, must be at least 2
+    """
+    print("*" * width) #print top edge of box
+
+    # print sides of box
+    for _ in range(height-2):
+        print("*" + " " * (width-2) + "*")
+
+    print("*" * width) #print bottom edge of box
